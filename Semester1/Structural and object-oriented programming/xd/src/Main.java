31e23aacb58fcdc40fe8bc71fe5c592a0ee69c0c
Kamotek <kamotek123@gmail.com>
2023-07-19 20:24:08 +0200

    Empty tasks deleted

--- Semester1/Structural and object-oriented programming/xd/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        klasa cos = new klasa(6);
-
-    }
-}
--- Semester1/Structural and object-oriented programming/xd/src/klasa.java
+++
@@ -1,7 +0,0 @@
-public class klasa {
-    private int xd;
-
-    public klasa(int n){
-        xd = n;
-    }
-}
